76f87ffd05dabd8f51e5129cc431a0647e58267b
Mohanedwael <mhndwayl47@gmail.com>
2024-09-19 02:26:06 +0300

    Add files via upload

--- game play.dart
+++ game play.dart
@@ -14,42 +14,177 @@ class gameplay extends StatelessWidget {
             color: Colors.black,
             fontFamily: 'cursive',
             fontWeight: FontWeight.bold,
-
           ),
         ),
+        centerTitle: true,
         leading: IconButton(
             onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.arrow_back_ios)),
 
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Row(
             children: [
-              Text("Player1",
-             style:
-             TextStyle(
-                  fontSize: 30,
-                  color: Colors.black,
-                  fontFamily: 'cursive',
-                  fontWeight: FontWeight.bold,
+              Expanded(
+                child: Center(
+                  child: Text("Player1",
+                               style:
+                               TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontFamily: 'cursive',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
-              Text("Player2",
-                style:
-                TextStyle(
-                  fontSize: 30,
-                  color: Colors.black,
-                  fontFamily: 'cursive',
-                  fontWeight: FontWeight.bold,
+              Expanded(
+                child: Center(
+                  child: Text("Player2",
+                    style:
+                    TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontFamily: 'cursive',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
-          Row(),
-          Row(),
-          Row(),
-          Row(),
+          Row(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text("0",
+                    style:
+                    TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontFamily: 'cursive',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text("0",
+                    style:
+                    TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontFamily: 'cursive',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  width: 150,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    border: Border.all(color: Colors.black,width:10)
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: 150,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      border: Border.all(color: Colors.black,width:10)
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: 150,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      border: Border.all(color: Colors.black,width:10)
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  width: 150,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      border: Border.all(color: Colors.black,width:10)
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: 150,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      border: Border.all(color: Colors.black,width:10)
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: 150,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      border: Border.all(color: Colors.black,width:10)
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  width: 150,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      border: Border.all(color: Colors.black,width:10)
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: 150,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      border: Border.all(color: Colors.black,width:10)
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: 150,
+                  height: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      border: Border.all(color: Colors.black,width:10)
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
