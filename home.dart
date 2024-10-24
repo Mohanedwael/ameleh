7886a9f90344fd6b9366fecae1bfd4970bfa3f60
Mohanedwael <mhndwayl47@gmail.com>
2024-10-24 21:37:19 +0300

    Add files via upload

---
+++ home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+class home extends StatefulWidget {
+  const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back,color: Colors.black,)
+        ),
+      ),
+    );
+  }
+}
